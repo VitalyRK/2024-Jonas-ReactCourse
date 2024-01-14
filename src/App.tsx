388cfce3dff6b8c263379712d5af7be1e9c756fb
VitalyRK <shishovip@yandex.ru>
2024-01-14 10:27:38 +0300

    feat: set up (Sun, Jan 14, 2024 10:27:38 AM)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import moment from 'moment';
+
+import './global.css';
+
 function App() {
+  const currentDate = moment().format('ddd MMM Do YYYY');
+  console.log(currentDate);
+
   return (
     <>
       <h1>My Personal Page</h1>
+      <h3>{currentDate}</h3>
     </>
   );
 }
