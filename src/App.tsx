734b08cdb9eaca31f2c7a99be49e365062fd1ee1
VitalyRK <shishovip@yandex.ru>
2024-01-28 19:45:08 +0300

    feat: complete (Sun, Jan 28, 2024  7:45:08 PM)

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,78 @@
-import About from './components/About/About';
-
 import './global.css';
 
-function App() {
-  return (
-    <>
-      <h1>My Educational Project</h1>
-      <About />
-    </>
-  );
+import { useState } from 'react';
+
+function useGeolocation() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [position, setPosition] = useState<{
+    lat: number;
+    lng: number;
+  }>({
+    lat: 0,
+    lng: 0,
+  });
+  const [error, setError] = useState<string>('');
+
+  function getPosition() {
+    if (!navigator.geolocation)
+      return setError('Your browser does not support geolocation');
+
+    setIsLoading(true);
+    navigator.geolocation.getCurrentPosition(
+      (pos) => {
+        setPosition({
+          lat: pos.coords.latitude,
+          lng: pos.coords.longitude,
+        });
+        setIsLoading(false);
+      },
+      (error) => {
+        setError(error.message);
+        setIsLoading(false);
+      }
+    );
+  }
+
+  return { isLoading, position, error, getPosition };
 }
 
-export default App;
+export default function App() {
+  const {
+    isLoading,
+    position: { lat, lng },
+    error,
+    getPosition,
+  } = useGeolocation();
+
+  const [countClicks, setCountClicks] = useState(0);
+
+  function handleClick() {
+    setCountClicks((count) => count + 1);
+    getPosition();
+  }
+
+  return (
+    <div>
+      <button onClick={handleClick} disabled={isLoading}>
+        Get my position
+      </button>
+
+      {isLoading && <p>Loading position...</p>}
+      {error && <p>{error}</p>}
+      {!isLoading && !error && lat !== 0 && lng !== 0 && (
+        <p>
+          Your GPS position:
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href={`https://www.openstreetmap.org/#map=16/${lat}/${lng}`}
+          >
+            {lat}, {lng}
+          </a>
+        </p>
+      )}
+
+      <p>You requested position {countClicks} times</p>
+    </div>
+  );
+}
